70c3365943bbd510efc05a676d5f948adcf2a50a
Dan Bachmann <nospam@DanBachmann.com>
2024-01-04 12:38:21 +0000

    added clean_geo & clean_user to shared file

--- DataBricks/PinterestTransformations.py
+++ DataBricks/PinterestTransformations.py
@@ -38,3 +38,42 @@ def clean_pin(df_pin):
         #     category
     df_pin = df_pin[['ind', 'unique_id', 'title', 'description', 'follower_count','poster_name', 'tag_list', 'is_image_or_video', 'image_src', 'save_location', 'category']]
     return df_pin
+
+def clean_geo(df_geo):
+    # Create a new column coordinates that contains an array based on the latitude and longitude columns
+    df_geo = df_geo.withColumn("coordinates", array("latitude", "longitude"))
+
+    # Drop the latitude and longitude columns from the DataFrame
+    df_geo = df_geo.drop("latitude", "longitude")
+    #df_geo.show(10)
+
+    # Convert the timestamp column from a string to a timestamp data type
+    df_geo = df_geo.withColumn("timestamp", to_timestamp("timestamp"))
+    df_geo.printSchema()
+
+    # Reorder the DataFrame columns to have the following column order:
+        # ind
+        # country
+        # coordinates
+        # timestamp
+    df_geo = df_geo['ind', 'country', 'coordinates', 'timestamp']
+    return df_geo
+
+def clean_user(df_user):
+    # Create a new column user_name that concatenates the information found in the first_name and last_name columns
+    df_user = df_user.withColumn("user_name", concat("first_name", "last_name"))
+
+    # Drop the first_name and last_name columns from the DataFrame
+    df_user = df_user.drop("first_name", "last_name")
+
+    # Convert the date_joined column from a string to a timestamp data type
+    df_user = df_user.withColumn("date_joined", to_timestamp("date_joined"))
+    df_user.printSchema()
+
+    # Reorder the DataFrame columns to have the following column order:
+    #     ind
+    #     user_name
+    #     age
+    #     date_joined
+    df_user = df_user['ind', 'user_name', 'age', 'date_joined']
+    return df_user
