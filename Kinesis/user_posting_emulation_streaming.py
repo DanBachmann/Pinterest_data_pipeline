5e11f958f20419803da52c966a20b77a9bed93ec
Dan Bachmann <nospam@DanBachmann.com>
2024-01-04 16:02:07 +0000

    removed Kinesis exploratory test code

--- Kinesis/user_posting_emulation_streaming.py
+++ Kinesis/user_posting_emulation_streaming.py
@@ -25,10 +25,9 @@ class AWSDBConnector:
 new_connector = AWSDBConnector()
 
 
-def post_data_to_api(data, suffix):
+def post_data_to_stream_api(data, suffix):
     aws_user = '0a1153066525'
     headers = {'Content-Type': 'application/json'}
-    data = {"index": 1, "name": "Dan", "age": 35, "role": "engineer"}
     payload = json.dumps({
         "StreamName": f"streaming-{aws_user}-{suffix}",
         "Data": data,
@@ -40,30 +39,6 @@ def post_data_to_api(data, suffix):
     print(response)
     print(response.content)
 
-# def post_data_to_api(data, suffix):
-#     aws_user = '0a1153066525'
-#     full_topic_name = f"{aws_user}.{topic_name}"
-#     api_invoke_url = "https://dc6k35vnyj.execute-api.us-east-1.amazonaws.com/staging/"
-#     invoke_url = f"{api_invoke_url}streams/streaming-0a1153066525-{suffix}/record"
-#     payload = json.dumps({
-#         "records": [
-#             {
-#             # Data should be send as pairs of column_name:value, with different columns separated by commas       
-#             "value": data
-#             }
-#         ]
-#     })
-
-    # headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
-    # response = requests.request("POST", invoke_url, headers=headers, data=payload)
-    # if response.status_code == 200:
-    #     print("Message sent successfully")
-    # else:
-    #     print(f"{response.status_code} when posting to: {invoke_url}")
-    #     print(payload)
-    #     print(response.text)
-    # return response
-
 def serialise_datetime(data, datetime_field):
     if datetime_field in data:
         data[datetime_field] = data[datetime_field].isoformat()
@@ -73,7 +48,7 @@ def serialise_datetime(data, datetime_field):
 def run_infinite_post_data_loop(max_count = -1):
     count = 0
     while max_count<0 or count < max_count:
-        sleep(random.randrange(0, 2))
+        sleep(random.randrange(0, 1))
         random_row = random.randint(0, 11000)
         engine = new_connector.create_db_connector()
 
@@ -98,18 +73,18 @@ def run_infinite_post_data_loop(max_count = -1):
                 user_result = dict(row._mapping)
             
             print(pin_result)
-            post_data_to_api(pin_result,'pin')
+            post_data_to_stream_api(pin_result,'pin')
             print('')
 
             print(geo_result)
             serialise_datetime(geo_result, 'timestamp')
             print(geo_result)
-            post_data_to_api(geo_result,'geo')
+            post_data_to_stream_api(geo_result,'geo')
             print('')
 
             print(user_result)
             serialise_datetime(user_result, 'date_joined')
-            post_data_to_api(user_result,'user')
+            post_data_to_stream_api(user_result,'user')
             print('-----------------------')
 
             count = count + 1
@@ -123,7 +98,7 @@ def dump_data_pin(max_count = -1):
         
         for row in pin_rows:
             pin_result = dict(row._mapping)
-            post_data_to_api(pin_result,'pin')
+            post_data_to_stream_api(pin_result,'pin')
             print(row)
 
 
@@ -137,7 +112,7 @@ def dump_data_geo(max_count = -1):
         for row in pin_rows:
             geo_result = dict(row._mapping)
             serialise_datetime(geo_result, 'timestamp')
-            post_data_to_api(geo_result,'geo')
+            post_data_to_stream_api(geo_result,'geo')
             print(row)
 
 def dump_data_user(max_count = -1):
@@ -150,34 +125,13 @@ def dump_data_user(max_count = -1):
         for row in pin_rows:
             user_result = dict(row._mapping)
             serialise_datetime(user_result, 'date_joined')
-            post_data_to_api(user_result,'user')
+            post_data_to_stream_api(user_result,'user')
             print(row)
 
-# def test_stream(suffix):
-#     aws_user = '0a1153066525'
-#     example_df = {"index": 1, "name": "Dan", "age": 35, "role": "engineer"}
-
-#     headers = {'Content-Type': 'application/json'}
-#     payload = json.dumps({
-#         "StreamName": f"streaming-{aws_user}-{suffix}",
-#         "Data":
-#                 example_df,
-#                 # {
-#                 # "index": example_df["index"], "name": example_df["name"], "age": example_df["age"], "role": example_df["role"]
-#                 # },
-#                 "PartitionKey": "partition-1"
-#                 })
-#     print(payload)
-#     # invoke url for one record, if you want to put more records replace record with records
-#     invoke_url = f"https://dc6k35vnyj.execute-api.us-east-1.amazonaws.com/staging/streams/streaming-{aws_user}-{suffix}/record"
-#     response = requests.request("PUT", invoke_url, headers=headers, data=payload)
-#     print(response)
-#     print(response.content)
 
 if __name__ == "__main__":
     print('Working')
-    # test_stream("pin")
-    dump_data_pin()
+    # dump_data_pin()
     # dump_data_geo()
     # dump_data_user()
-    #run_infinite_post_data_loop(1)
+    run_infinite_post_data_loop()
